5ea1f558e44e8ab5e7fd7290d5e5cda7c009aa75
nullLuli <2323598470@qq.com>
2018-01-27 17:19:51 +0800

    【加载页面所有annotate】

--- ReaderDemo/OutlineController.m
+++ ReaderDemo/OutlineController.m
@@ -8,7 +8,7 @@
 
 #import "OutlineController.h"
 
-@interface OutlineController ()
+@interface OutlineController ()<UITableViewDelegate,UITableViewDataSource>
 @property (weak, nonatomic) IBOutlet UIView *contentView;
 
 @property(nonatomic,strong)UITableView * tableView;
@@ -21,15 +21,60 @@
     [super viewDidLoad];
 }
 
+-(void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+    [self.tableView reloadData];
+}
+
+-(void)viewDidLayoutSubviews
+{
+    [super viewDidLayoutSubviews];
+    
+    self.tableView.frame = self.contentView.bounds;
+}
+
+-(void)setAnnotationArray:(NSArray<PDFAnnotation *> *)annotationArray
+{
+    _annotationArray = annotationArray;
+    
+    [self.tableView reloadData];
+}
+
 -(UITableView *)tableView
 {
     if (self.contentView && !_tableView) {
-//        _tableView = [UITableView alloc]initWithFrame: style:<#(UITableViewStyle)#>
+        _tableView = [[UITableView alloc]initWithFrame:CGRectZero style:UITableViewStylePlain];
+        _tableView.delegate = self;
+        _tableView.dataSource = self;
+        _tableView.rowHeight = 70;
+        [_tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"UITableViewCell"];
+        [self.contentView addSubview:_tableView];
     }
+    return _tableView;
 }
 
 - (IBAction)dismiss
 {
-    [self.presentedViewController dismissViewControllerAnimated:self completion:nil];
+    [self.presentingViewController dismissViewControllerAnimated:self completion:nil];
+}
+
+#pragma mark UITableViewDataSource
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.annotationArray.count;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:@"UITableViewCell" forIndexPath:indexPath];
+    cell.textLabel.text = self.annotationArray[indexPath.row].type;
+    return cell;
 }
 @end
